f8ea400b990fabe6aa226efb3fd855e0ea59cf46
Maryanne Anyango <mayannanneanyango98@gmail.com>
2022-06-16 18:42:41 +0300

    files

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+
+document.addEventListener("DOMContentLoaded", () => {
+  const textPara = document.querySelector("#text")
+  textPara.textContent = "This is really cool!"
+  console.log(textPara);
+})
